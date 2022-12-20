9f259a169c7bf2bbf38f8efcdf58f0ec919d8d18
ching1es <99396116+ching1es@users.noreply.github.com>
2022-12-21 00:17:37 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -12,7 +12,6 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement; 
-    console.dir(li);
     li.remove();
     toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
     saveToDos();
